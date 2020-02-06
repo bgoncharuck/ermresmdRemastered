05a248001e2b96c9d26b85b8111c483ee1749852
Bohdan Honcharuk <dariydragon26@gmail.com>
2020-02-06 18:50:04 +0200

    learning kotlin 1

--- ermesRemastered.kt
+++ ermesRemastered.kt
@@ -24,6 +24,45 @@ enum class RarityOfItem(val rarityValue: Byte) {
   Legendary(8)
 }
 
-class magicEnchantment() {
-  
+enum class AttributeCode(val attributeCodeValue: Byte) {
+  health(-126),
+  stamina(-125),
+  magic(-124),
+  healthregen(-123),
+  staminaregen(-122),
+  magickaregen(-121),
+  hunger(-120),
+  strength(42),
+  intelligence(43),
+  willpower(44),
+  agility(45),
+  speed(46),
+  endurance(47),
+  personality(48),
+  reputation(49),
+  bounty(50)
+}
+
+interface Attribute(val _attributeCode: Byte) {
+  val attributeCode: Byte= _attributeCode
+  fun value()
+}
+
+class AttributeVal(val _attributeCode: Byte, val _value: Double): Attribute(_attributeCode) {
+  val value: Double= _value
+  override fun value() {
+    return value
+  }
+}
+
+class AttributeVar(val _attributeCode: Byte, val _value: Double): Attribute(_attributeCode) {
+  var value: Double= _value
+  override fun value() {
+    return value
+  }
+}
+
+class magicEnchantment(val _name: String, val _ench: Attribute) {
+  val name: String= _name
+  val enchantment: Attribute= _ench
 }
