2f950323621fe6ff7d4199cf347ff700cf488730
Bohdan Honcharuk <dariydragon26@gmail.com>
2020-02-07 01:16:32 +0200

    .gitignore new

--- ermesRemastered.kt
+++ ermesRemastered.kt
@@ -32,6 +32,7 @@ enum class AttributeCode(val code: Byte) {
   staminaregen(-122),
   magickaregen(-121),
   hunger(-120),
+  none(0),
   strength(42),
   intelligence(43),
   willpower(44),
@@ -48,9 +49,79 @@ interface Attribute {
   val value: Double
 }
 
+class NoneAttribute(): Attribute {
+  override val attributeCode: Byte=AttributeCode.none.code
+  override val value: Double=0.0
+}
+
 class Health(val _value: Double): Attribute {
   override val attributeCode: Byte=AttributeCode.health.code
   override val value: Double=_value
+}
+
+class Stamina(val _value: Double): Attribute {
+
+}
+
+class Magic(val _value: Double): Attribute {
+
+}
+
+class HealthRegen(val _value: Double): Attribute {
+
+}
+
+class StaminaRegen(val _value: Double): Attribute {
+
+}
+
+class MagickaRegen(val _value: Double): Attribute {
+
+}
+
+class Hunger(val _value: Double): Attribute {
+
+}
+
+class Strength(val _value: Double): Attribute {
+
+}
+
+class Intelligence(val _value: Double): Attribute {
+
+}
+
+class Willpower(val _value: Double): Attribute {
+
+}
+
+class Agility(val _value: Double): Attribute {
+
+}
+
+class Speed(val _value: Double): Attribute {
+
+}
+
+class Endurance(val _value: Double): Attribute {
+
+}
+
+class Personality(val _value: Double): Attribute {
+
+}
+
+class Reputation(val _value: Double): Attribute {
+
+}
+
+class Bounty(val _value: Double): Attribute {
+
+}
+
+interface Enchantment {
+  val name: String
+  val context: Attribute
 }
 
 fun main() {
